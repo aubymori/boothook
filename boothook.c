9d3206b5718f2ef552e871be71b8058ed535248b
aubymori <aubyomori@gmail.com>
2024-10-07 13:03:42 -0500

    Init

---
+++ boothook.c
@@ -0,0 +1,15 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at htp://mozilla.org/MPL/2.0/. */
+#include <ntddk.h>
+
+NTSTATUS DriverEntry(
+	_In_ PDRIVER_OBJECT DriverObject,
+	_In_ PUNICODE_STRING RegistryPath
+)
+{
+	UNREFERENCED_PARAMETER(DriverObject);
+	UNREFERENCED_PARAMETER(RegistryPath);
+
+	return STATUS_SUCCESS;
+}
